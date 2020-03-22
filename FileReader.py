95cfa4b70d08f431ab25cb66ac5c7cd113e3b25d
Adrien <adrien@fake.com>
2020-03-22 14:16:08 -0400

    File Reader first

--- FileReader.py
+++ FileReader.py
@@ -1,4 +1,25 @@
 import sys
 
-print("Test")
+
+
+
+
+def filecontent(filename):
+    # Open a file
+    fo = open(filename, "r")
+    
+
+    # Assuming file has following 5 lines
+    # This is 1st line
+    # This is 2nd line
+    # This is 3rd line
+    # This is 4th line
+    # This is 5th line
+
+    line = fo.read()
+    
+
+    # Close opend file
+    fo.close()
+    return line 
 
